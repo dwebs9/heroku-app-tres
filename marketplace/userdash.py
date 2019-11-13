cba1e79d3c9ab90b4ca0f0752c7e312caf7991ec
David <dwebs9@live.com>
2019-11-13 12:34:47 +1000

    re-organization of userdash feature

--- marketplace/userdash.py
+++ marketplace/userdash.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-from flask import (Blueprint, flash, render_template, session,
-                   request, url_for, redirect)
+from flask import Blueprint, flash, render_template, session, request, url_for, redirect
 from .models import Tool, Bid, User
 from sqlalchemy import desc, func
 from .forms import BidForm, MarkSold, UndoSold, CreateForm
@@ -10,10 +9,10 @@ from decimal import Decimal, getcontext
 import os
 from . import db
 
-bp = Blueprint('userdash', __name__, url_prefix='/userdash')
+bp = Blueprint("userdash", __name__, url_prefix="/userdash")
 
 
-@bp.route('/main/<userid>', methods=["POST", "GET"])
+@bp.route("/main/<userid>", methods=["POST", "GET"])
 @login_required
 def maindash(userid):
 
@@ -24,7 +23,7 @@ def maindash(userid):
     print(tool_length)
     print(userid)
     # tool_length = session.get('tool_length', None)
-
+    # arbitrary comment
     # count bids made by user
     bids = db.session.query(Tool, Bid).join(Bid).filter_by(user_id=userid).all()
     bid_length = len(bids)
@@ -39,36 +38,47 @@ def maindash(userid):
         total = total + i.list_price
 
     # recently viewed items list
-    recently_viewed = session.get('vieweditems')
+    recently_viewed = session.get("vieweditems")
     views = Tool.query.filter(Tool.id.in_(recently_viewed)).limit(5).all()
 
-    return render_template('userdash/maindash.html', userid=userid, tool_length=tool_length, bid_length=bid_length, sold_length=sold_length, views=views, total=total)
+    return render_template(
+        "userdash/maindash.html",
+        userid=userid,
+        tool_length=tool_length,
+        bid_length=bid_length,
+        sold_length=sold_length,
+        views=views,
+        total=total,
+    )
+
 
 # Items user is selling
-@bp.route('/userselling/<userid>', methods=["POST", "GET"])
+@bp.route("/userselling/<userid>", methods=["POST", "GET"])
 @login_required
 def userselling(userid):
 
     # query db for tools current user has listed
     tool = Tool.query.filter_by(user_id=userid).filter(Tool.sold_status == "0").all()
 
-    return render_template('userdash/manageselling.html', userid=userid, tool=tool)
+    return render_template("userdash/manageselling.html", userid=userid, tool=tool)
+
 
 # Items user has sold
-@bp.route('/usersold/<userid>', methods=["POST", "GET"])
+@bp.route("/usersold/<userid>", methods=["POST", "GET"])
 @login_required
 def usersold(userid):
 
     sold = Tool.query.filter_by(user_id=userid).filter(Tool.sold_status != "0").all()
-    
-    return render_template('userdash/managesold.html', userid=userid, sold=sold)
+
+    return render_template("userdash/managesold.html", userid=userid, sold=sold)
+
 
 # Items user has bid on
-@bp.route('/userbids/<userid>', methods=["POST", "GET"])
+@bp.route("/userbids/<userid>", methods=["POST", "GET"])
 @login_required
 def userbids(userid):
 
     # query db for bids current user has made
     bids = db.session.query(Tool, Bid).join(Bid).filter_by(user_id=userid).all()
 
-    return render_template('userdash/managebids.html', userid=userid, bids=bids)
+    return render_template("userdash/managebids.html", userid=userid, bids=bids)
